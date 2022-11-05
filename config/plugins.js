135a347d06899f949a2b3256575645057a854736
Quentin Burgniard <quentinburgniard@gmail.com>
2022-11-05 15:26:09 +0100

    feat(email & upload)

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,35 @@
 const crypto = require('crypto');
 
-module.exports = {
+module.exports = ({ env }) => ({
+  'email': {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_TOKEN')
+      },
+      settings: {
+        defaultFrom: 'bot@digitalleman.com',
+        defaultReplyTo: 'hello@digitalleman.com'
+      }
+    }
+  },
+  'upload': {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('DIGITALOCEAN_USER'),
+        endpoint: 'https://fra1.digitaloceanspaces.com',
+        params: {
+          Bucket: 'digitalleman'
+        },
+        region: 'fra1',
+        secretAccessKey: env('DIGITALOCEAN_PASSWORD')
+      }
+    }
+  },
   'users-permissions': {
     config: {
       jwtSecret: crypto.randomBytes(32).toString('hex')
     }
   }
-};
+});
