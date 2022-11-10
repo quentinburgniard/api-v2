b64600e3de3d7a9eec0ab2916ceddf715ff67c8c
Quentin Burgniard <quentinburgniard@gmail.com>
2022-11-10 16:45:30 +0100

    fix(paragraph)

--- src/api/paragraph/controllers/paragraph.js
+++ src/api/paragraph/controllers/paragraph.js
@@ -8,7 +8,7 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::paragraph.paragraph', ({ strapi }) => ({
   async find(ctx) {
-    console.log(ctx);
+    console.log(ctx.state);
     const { data, meta } = await super.find(ctx);
     return { data, meta };
   }
