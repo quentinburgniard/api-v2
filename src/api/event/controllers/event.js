5ecca7ff113f40dbbd7e2869085ea9972c1540e9
Quentin Burgniard <quentinburgniard@gmail.com>
2022-12-30 21:00:33 +0100

    feat(event): add user

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -1,9 +1,22 @@
 'use strict';
 
-/**
- * presentation controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController('api::event.event', ({ strapi }) => ({
+  async create(ctx) {
+    if (ctx.state.user) {
+      ctx.request.body.data.user = ctx.state.user.id;
+      const response = await super.create(ctx);
+      return response;
+    }
+  },
+  async find(ctx) {
+    if (ctx.state.user) {
+      let filters = ctx.query.filters || {};
+      filters.user = ctx.state.user.id;
+      ctx.query.filters = filters;
+      const { data, meta } = await super.find(ctx);
+      return { data, meta };
+    }
+  }
+}));
