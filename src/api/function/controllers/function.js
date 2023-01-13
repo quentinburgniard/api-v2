3858ba5c257ef456c9dc89ebf709a8f731c789ad
Quentin Burgniard <quentinburgniard@gmail.com>
2023-01-13 14:34:10 +0100

    fix(function)

--- src/api/function/controllers/function.js
+++ src/api/function/controllers/function.js
@@ -8,11 +8,10 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::function.function', ({ strapi }) => ({
   async create(ctx) {
-    /*if (ctx.state.user) {
-      ctx.request.body.data.user = ctx.state.user.id;*/
-      const response = await strapi.service('api::function.function').screenshot(ctx.request.body.data.parameters);
-      await super.create(ctx);
+    if (ctx.state.user) {
+      ctx.request.body.data.user = ctx.state.user.id;
+      const response = await super.create(ctx);
       return response;
-    //}
+    }
   }
 }));
