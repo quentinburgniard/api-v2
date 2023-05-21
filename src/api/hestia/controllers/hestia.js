d16f5928672171da1a821e561c1eb76ac179846a
Quentin Burgniard <quentinburgniard@gmail.com>
2023-05-21 13:04:25 +0200

    feat: rename hestia to valerie

--- src/api/hestia/controllers/hestia.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * hestia controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::hestia.hestia');
--- src/api/hestia/routes/hestia.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * hestia router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::hestia.hestia');
