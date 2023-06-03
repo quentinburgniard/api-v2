ca171948e280eb2da0655a228d77f66d5631be15
Quentin Burgniard <quentinburgniard@gmail.com>
2023-06-03 23:52:49 +0200

    feat(assets): getContent

--- src/api/asset/controllers/asset.js
+++ src/api/asset/controllers/asset.js
@@ -13,13 +13,6 @@ const s3 = new S3Client({
   }
 });
 
-const streamToString = (stream) => new Promise((resolve, reject) => {
-  const chunks = [];
-  stream.on('data', (chunk) => chunks.push(chunk));
-  stream.on('error', reject);
-  stream.on('end', () => resolve(Buffer.concat(chunks)));
-});
-
 module.exports = createCoreController('api::asset.asset', ({ strapi }) =>  ({
   async findOneBySlug(context) {
     const results = await strapi.service('api::asset.asset').findOneBySlug(context.params.slug, context.state.user ? context.state.user.id : null);
@@ -28,17 +21,17 @@ module.exports = createCoreController('api::asset.asset', ({ strapi }) =>  ({
   },
 
   async getContent(context) {
-    s3.send(new GetObjectCommand({
-      Bucket: 'digitalleman',
-      Key: `api/${context.params.slug}`
-    }))
-    .then((data) => {
-      context.type = data.ContentType;
-      context.set('cache-control', 'public, max-age=86400');
-      context.set('cache-control', 'private, max-age=86400');
-      streamToString(data.Body).then((data) => {
-        context.body = data;
-      });
-    });
+    const results = await strapi.service('api::asset.asset').findOneBySlug(context.params.slug, context.state.user ? context.state.user.id : null);
+    console.log(results);
+    if (results) {
+      const response = await s3.send(new GetObjectCommand({
+        Bucket: 'digitalleman',
+        Key: `api/${context.params.slug}`
+      }));
+      context.body = Buffer.concat(await response.Body.toArray());
+      context.status = 200;
+      context.type = response.ContentType;
+      context.set('cache-control', `${results.public ? 'public': 'private'}, max-age=604800`);
+    }
   }
 }));
