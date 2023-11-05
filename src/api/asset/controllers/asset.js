f261ed99aaf6fb7c7b3de75bc76e20e0b3513483
Quentin Burgniard <quentinburgniard@gmail.com>
2023-11-05 12:00:48 +0100

    feat(asset)

--- src/api/asset/controllers/asset.js
+++ src/api/asset/controllers/asset.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const { createCoreController } = require('@strapi/strapi').factories;
-const { GetObjectCommand, S3Client } = require('@aws-sdk/client-s3');
+const { GetObjectCommand, PutObjectCommand, S3Client } = require('@aws-sdk/client-s3');
+const { createReadStream } = require('fs');
 
 const s3 = new S3Client({
   forcePathStyle: false,
@@ -14,6 +15,18 @@ const s3 = new S3Client({
 });
 
 module.exports = createCoreController('api::asset.asset', ({ strapi }) =>  ({
+  async create(ctx) {
+    console.log(ctx.request.files.file);
+    const response = await s3.send(new PutObjectCommand({
+      Body: createReadStream(ctx.request.files.file.path),
+      Bucket: 'digitalleman',
+      ContentType: ctx.request.files.file.type,
+      Key: `api/${ctx.request.files.file.name}`
+    }));
+    //const response = await super.create(ctx);
+    //return response;
+  },
+  
   async findOneBySlug(context) {
     const results = await strapi.service('api::asset.asset').findOneBySlug(context.params.slug, context.state.user ? context.state.user.id : null);
     const sanitizedResults = await this.sanitizeOutput(results, context);
